c6f7d3e50d065210ad024b84d7685c94711af630
pibo <pibomusic@gmail.com>
2019-07-18 02:34:03 +0900

    選択したコードをマネージャーに伝える

--- Assets/domain/chordPad/ChordPadManager.cs
+++ Assets/domain/chordPad/ChordPadManager.cs
@@ -11,6 +11,8 @@ public class ChordPadManager : MonoBehaviour
         12, 24, 28, 31
     };
 
+    private Chord nowChord;
+
     MidiChannel channel = MidiChannel.Ch1;
 
     void Start()
@@ -18,7 +20,23 @@ public class ChordPadManager : MonoBehaviour
         //TODO: ここ関数にする
         GameObject chordSelecter = Instantiate(chordSelecterPrefab, new Vector3(0f, 0f, 0f), Quaternion.identity);
         ChordSelecter cs1 = chordSelecter.GetComponent<ChordSelecter>();
-        cs1.Create(chord: new Chord(PitchName.C, Type.maj));
+        cs1.Create(chord: new Chord(PitchName.C, Type.maj), creator: this);
+
+        GameObject chordSelecter2 = Instantiate(chordSelecterPrefab, new Vector3(1f, 0f, 0f), Quaternion.identity);
+        ChordSelecter cs2 = chordSelecter2.GetComponent<ChordSelecter>();
+        cs2.Create(chord: new Chord(PitchName.D, Type.min), creator: this);
+
+        GameObject chordSelecter3 = Instantiate(chordSelecterPrefab, new Vector3(2f, 0f, 0f), Quaternion.identity);
+        ChordSelecter cs3 = chordSelecter3.GetComponent<ChordSelecter>();
+        cs3.Create(chord: new Chord(PitchName.E, Type.min), creator: this);
+    }
+
+    public void setChord(Chord receivedChord)
+    {
+        //多分だけどここにイベント入れておくとこのイベントに登録したGraphicとかOSCのOutができる気がする
+        this.nowChord = receivedChord;
+        Debug.Log(this.nowChord.root);
+        Debug.Log(this.nowChord.type[0]);
     }
     public void Ring(float velocity)//velocity range: 0~1f
     {
--- Assets/domain/chordPad/ChordSelecter.cs
+++ Assets/domain/chordPad/ChordSelecter.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * ChordSelecter
+ * ChordPadManagerによって自動生成される
+ * 生成される時点でユニークなコードネームが割り当てられる
+ * isTriggerを持つRigidBodyが自身のColliderに侵入するとChordPadManagerにコードの変更を伝える
+ */
 public class ChordSelecter : MonoBehaviour
 {
     public Chord chord;
-    public void Create(Chord chord)
+    private ChordPadManager creator;
+    public void Create(Chord chord, ChordPadManager creator)
     {
         this.chord = chord;
-        Debug.Log(this.chord.root);
-        Debug.Log(this.chord.type[0]);
+        this.creator = creator;
+        Debug.Log(chord.root);
+    }
+
+    void OnTriggerEnter()
+    {
+        creator.setChord(chord);
+        Debug.Log("trigger haitta" + creator);
     }
     // Use this for initialization
     void Start()
